70ae98074942b030a1bb095e47f65817798d36fb
Asitkdey540 <dasit520@gmail.com>
2022-04-17 22:39:05 +0600

    assignment done without bonus mark (bonus mark part will be added)

--- js/main.js
+++ js/main.js
@@ -1,1 +1,26 @@
-console.log("Asaignment two")
+function priceAdder(id, price) {
+    // extraMemoryCost, extraStorageCost, deliveryCost
+
+    // All ID selected and their inner text stored into variables.
+    const bestPrice = document.getElementById('bestPrice');
+    const extraMemory = parseInt(document.getElementById('extraMemoryCost').innerText);
+    const extraStorage = parseInt(document.getElementById('extraStorageCost').innerText);
+    const deliveryCost = parseInt(document.getElementById('deliveryCost').innerText);
+    const totalCost = document.getElementById('totalCost');
+
+    // Initializing extra prices to it's exact position based on the "id" parameter.
+
+    const extraPrices = document.getElementById(id);
+    extraPrices.innerText = price;
+
+    // Counting total cost from extra memory, extra storage, delivery cost's inner test and the best price 1299.
+
+    const total = extraMemory + extraStorage + deliveryCost + 1299;
+
+    // Writing total prices to best price and total price section.
+
+    bestPrice.innerText = total;
+
+    totalCost.innerText = total;
+}
+
