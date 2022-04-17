247f3669797c46d8505ec3f4fc0e450dfe573fc2
Asitkdey540 <dasit520@gmail.com>
2022-04-18 00:02:12 +0600

    bonus mark added and full assignment complete

--- js/main.js
+++ js/main.js
@@ -1,12 +1,10 @@
 function priceAdder(id, price) {
     // extraMemoryCost, extraStorageCost, deliveryCost
 
-    // All ID selected and their inner text stored into variables.
+    // All ID selected.
     const bestPrice = document.getElementById('bestPrice');
-    const extraMemory = parseInt(document.getElementById('extraMemoryCost').innerText);
-    const extraStorage = parseInt(document.getElementById('extraStorageCost').innerText);
-    const deliveryCost = parseInt(document.getElementById('deliveryCost').innerText);
     const totalCost = document.getElementById('totalCost');
+    const finalTotal = document.getElementById('finalTotal');
 
     // Initializing extra prices to it's exact position based on the "id" parameter.
 
@@ -15,12 +13,27 @@ function priceAdder(id, price) {
 
     // Counting total cost from extra memory, extra storage, delivery cost's inner test and the best price 1299.
 
-    const total = extraMemory + extraStorage + deliveryCost + 1299;
+    const extraMemory = parseInt(document.getElementById('extraMemoryCost').innerText);
+    const extraStorage = parseInt(document.getElementById('extraStorageCost').innerText);
+    const deliveryCost = parseInt(document.getElementById('deliveryCost').innerText);
 
+    const total = extraMemory + extraStorage + deliveryCost + 1299;
     // Writing total prices to best price and total price section.
 
     bestPrice.innerText = total;
-
     totalCost.innerText = total;
+    finalTotal.innerText = total;
+}
+
+
+function discountKupon() {
+    const kupon = document.getElementById('kupon');
+    const finalTotal = document.getElementById('finalTotal');
+    let parseFinalPrice = parseInt(finalTotal.innerText);
+
+    if (kupon.value === "stevekaku") {
+        kupon.value = "";
+        finalTotal.innerText = parseFinalPrice - parseFinalPrice * (20 / 100);
+    }
 }
 
